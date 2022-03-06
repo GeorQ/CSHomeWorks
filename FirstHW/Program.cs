7c49d63b2d2bc7c57c2e99dc6c38b8b2537a20ca
GeorQ <georqv2@icloud.com>
2022-03-06 17:47:46 +0000

    Finish first hw

---
+++ FirstHW/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FirstHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMessage(false);
+            Console.WriteLine();
+            PrintMessage(true);
+
+            Console.ReadKey();
+        }
+
+        static void PrintMessage(bool isNewLine)
+        {
+            if (!isNewLine)
+            {
+                Console.WriteLine("Hello world!!!");
+                return;
+            }
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+        }
+    }
+}
